05c31ff929bc16d0bc24415cc98c16050906a78e
Mathieu Vedie <mathieu.vedie@gmail.com>
2022-06-16 12:03:23 +0200

    S

--- src/main/java/com/vocapia/voxsigma/Option.java
+++ src/main/java/com/vocapia/voxsigma/Option.java
@@ -17,26 +17,34 @@ abstract public class Option {
 
     /**
      * Render the option as a query string to generate the API Rest endpoint URL
+     *
      * @return String
      */
     public String renderQueryString() {
-        return URLEncoder.encode(this.name, StandardCharsets.UTF_8)+"="+URLEncoder.encode(this.value, StandardCharsets.UTF_8);
+        try {
+            return URLEncoder.encode(this.name, StandardCharsets.UTF_8.toString()) + "=" + URLEncoder.encode(this.value, StandardCharsets.UTF_8.toString());
+        } catch (UnsupportedEncodingException ignored) {
+        }
+        return "";
     }
 
     /**
      * Return the size of content added to a multipart request body
+     *
      * @return long
      */
     abstract public long getHttpBodySize();
 
     /**
      * Return the header for a multipart request body part
+     *
      * @return String
      */
     abstract public String getMultipartBodyHeader();
 
     /**
      * Write the content of the request body in the output stream
+     *
      * @param outputStream
      */
     abstract public void renderHttpBody(OutputStream outputStream);
